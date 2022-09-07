b06304a6609a7dfb2901b48309352dcddca306bf
wcocuzza <wallace.cocuzza@gmail.com>
2022-09-07 13:52:54 -0300

    Turned to installable binary #9

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(
     description="Reward Point System Dunder Mifflin",
     author="Wallace Cocuzza",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "dundie = dundie.__main__:main"
+        ]
+    },
 )
