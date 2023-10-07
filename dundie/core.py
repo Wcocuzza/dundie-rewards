612393a6eeefe9a530c962662bcfe876e8d10554
wcocuzza <wallace.cocuzza@gmail.com>
2023-10-07 12:52:36 -0300

    feat: add ci gha

--- dundie/core.py
+++ dundie/core.py
@@ -4,9 +4,13 @@ from dundie.utils.log import get_logger
 log = get_logger()
 
 def load(filepath):
-    """Loads data from filepath to the database"""
+    """Loads data from filepath to the database.
+    >>> len(load('assets/people.csv'))
+    2
+    """
     try:
         with open(filepath) as file_:
             return [line.strip() for line in file_.readlines()]
     except FileNotFoundError as e:
         log.error(str(e))
+        raise e
--- integration/conftest.py
+++ integration/conftest.py
@@ -1,13 +1,10 @@
-MARKER = """
-integration: Mark integration tests
+MARKER = """\
 unit: Mark unit tests
+integration: Mark integration tests
 high: High Priority
 medium: Medium Priority
 low: Low Priority
 """
 
 def pytest_configure(config):
-    map(lambda line: 
-        config.addinivalue_line('markers', line),
-        MARKER.split("\n")
-    )
+    map(lambda line: config.addinivalue_line('markers', line), MARKER.split("\n"))
