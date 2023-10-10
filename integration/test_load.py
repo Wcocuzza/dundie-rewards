92adec26f20a68757b697872dccf2c809d69f7fb
wcocuzza <wallace.cocuzza@gmail.com>
2023-10-10 20:17:37 -0300

    add isort, black

--- integration/test_load.py
+++ integration/test_load.py
@@ -1,15 +1,19 @@
+from subprocess import CalledProcessError, check_output
+
 import pytest
-from subprocess import check_output, CalledProcessError
 
 
 @pytest.mark.integration
 @pytest.mark.medium
 def test_load_positive_call_command():
     """test command load"""
-    out = check_output(
-        ["dundie", "load", "integration/assets/people.csv"]
-    ).decode("utf-8").split("\n")
+    out = (
+        check_output(["dundie", "load", "integration/assets/people.csv"])
+        .decode("utf-8")
+        .split("\n")
+    )
     assert len(out) == 2
+
 
 @pytest.mark.integration
 @pytest.mark.medium
@@ -17,8 +21,10 @@ def test_load_positive_call_command():
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """test command load"""
     with pytest.raises(CalledProcessError) as error:
-        out = check_output(
-            ["dundie", wrong_command, "integration/assets/people.csv"]
-        ).decode("utf-8").split("\n")
+        out = (
+            check_output(["dundie", wrong_command, "integration/assets/people.csv"])
+            .decode("utf-8")
+            .split("\n")
+        )
 
     assert "status 2" in str(error.getrepr())
